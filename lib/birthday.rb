5e39f5c867c936d233fcb0926c122e20b7bc26fd
Nattida Wong <nattidac.wong@gmail.com>
2020-09-19 20:55:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,7 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(kid)
+def age_appropriate_birthday(kids)
   kids.each do |kids_name, age|
     if age >= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
